c624a11d8e7699e859bdba2bfaad833baa695b93
Anastasiya Balakhomskaya <nstdigital21@gmail.com>
2022-08-28 12:48:17 +0300

    Add form home about contact

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title-block')О нас@endsection
+
+@section('content')
+    <h1>О нас</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aliquid architecto ex itaque minima natus odit porro, quam.
+        Adipisci, atque autem culpa inventore neque porro praesentium quam quos repellendus reprehenderit voluptas!</p>
+@endsection
+
+
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title-block')Обратная связь@endsection
+
+@section('content')
+<h1>Обратная связь</h1>
+    <form action="/contact/submit" method="post">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Введите имя </label>
+            <input type="text" name="name" placeholder="Введите имя" id="name" class="form=control">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="email">Введите email </label>
+            <input type="text" name="email" placeholder="Введите email" id="email" class="form=control">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="phone">Введите телефон</label>
+            <input type="text" name="phone" placeholder="Введите телефон" id="phone" class="form=control">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-success">Отправить</button>
+
+
+    </form>
+
+@endsection
