ff526a2515639d03b6d5a4121b0d5a1c8f3e077c
Anastasiya Balakhomskaya <nstdigital21@gmail.com>
2022-11-05 10:39:05 +0300

    fixed error hw_25_react

--- frontend-app/src/TemperatureControl.js
+++ frontend-app/src/TemperatureControl.js
@@ -1,49 +1,36 @@
 import { useState } from "react";
 
 function TemperatureControl() {
-    const [count, setTemp] = useState(10);
-    const maxTemp = 30;
-    const minTemp = 0;
-       
-const tempUp = () => {
-    if (count < maxTemp)
-     setTemp(count + 1);
-    
-}
+  const [count, setTemp] = useState(10);
+  const maxTemp = 30;
+  const minTemp = 0;
 
-const tempDown = () => {
+  const tempUp = () => {
+    if (count < maxTemp)
+      setTemp(count + 1);
+
+  }
+
+  const tempDown = () => {
     if (count > minTemp)
       setTemp(count - 1);
-}
+  }
 
-if (count >= 15)
-return ( 
-<div class="app-container">
-  <div class="temperature-display-container">
-    <div class="temperature-display hot">   
-      <p>{ count }</p>
-    </div>   
-  </div>
-     <div class="button-container">
-            <button onClick={tempUp}>+</button>
-            <button onClick={tempDown}>-</button>
-     </div>
-</div>
-);
-else
-return ( 
-    <div class="app-container">
-      <div class="temperature-display-container">
-        <div class="temperature-display cold">   
-          <p>{ count }</p>
-        </div>   
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${count >= 15 ? 'hot' : 'cold'}`}>
+          <p>{count}</p>
+        </div>
       </div>
-         <div class="button-container">
-                <button onClick={tempUp}>+</button>
-                <button onClick={tempDown}>-</button>
-         </div>
+      <div className="button-container">
+        <button onClick={tempUp}>+</button>
+        <button onClick={tempDown}>-</button>
+      </div>
     </div>
- );
+  );
+
 }
 
 export default TemperatureControl;
--- frontend-app/src/ToDoList.js
+++ frontend-app/src/ToDoList.js
@@ -14,17 +14,17 @@ function ToDoList() {
             return;
         }
 
-        const newToDo = [...todos, {value: input, isDone: false}];
+        const newToDo = [...todos, { value: input, isDone: false }];
         setToDo(newToDo);
         setInput('');
     }
 
     const deleteToDo = index => {
-        const newToDos =[...todos];
-        newToDos.splice(index,1);
+        const newToDos = [...todos];
+        newToDos.splice(index, 1);
         setToDo(newToDos);
     };
-    
+
     const deleteToDoAll = () => {
         setToDo([]);
     }
@@ -38,29 +38,28 @@ function ToDoList() {
 
     return (
         <div className="container">
-            <h1 className="text-center">To Do List</h1>           
+            <h1 className="text-center">To Do List</h1>
             <div className="justify-content-center">
                 <form className="input-group" onSubmit={addToDo} >
-                    <input onChange={onInputChange} value={input} type="text" className="form-control" />                 
+                    <input onChange={onInputChange} value={input} type="text" className="form-control" />
                     <div className="input-group-append">
                         <button className="input-group-text">Add</button>
                     </div>
                 </form>
                 <div >
                     <ul className="list-group">
-                    
-                        {todos.map((todo, index) =>   
-                        <ToDo
-                        key={index} 
-                        todo={todo}
-                        index={index}                       
-                        toggle={() => toggleComplete(index)} 
-                        value={todo.value} 
-                        isDone={todo.isDone}
-                        del={deleteToDo}
-                        />)                  
-                        }          
-                    </ul>                    
+
+                        {todos.map((todo, index) =>
+                            <ToDo
+                                key={index}
+                                todo={todo}
+                                toggle={() => toggleComplete(index)}
+                                value={todo.value}
+                                isDone={todo.isDone}
+                                del={() => deleteToDo(index)}
+                            />)
+                        }
+                    </ul>
                 </div>
             </div>
             <button className="input-group-text" onClick={deleteToDoAll}>Delete all</button>
@@ -68,18 +67,18 @@ function ToDoList() {
     );
 }
 
-function ToDo({value, isDone, toggle, del, index}) {
-   
-    return ( 
-        
-        <ul className={isDone ? "backgroudToDo" :"list-group-item"}>
-          <input onChange={toggle} className="form-check-input" type="checkbox"/>
-          <div checked className={isDone ? "todo_done" : ""}>
-           {value}
-          </div> 
-          <button onClick={()=>del(index)} className="input-group-text">Delete ToDo</button>       
-        </ul>         
-    ); 
+function ToDo({ value, isDone, toggle, del, index }) {
+
+    return (
+
+        <ul className={isDone ? "backgroudToDo" : "list-group-item"}>
+            <input onChange={toggle} className="form-check-input" type="checkbox" />
+            <div checked className={isDone ? "todo_done" : ""}>
+                {value}
+            </div>
+            <button onClick={() => del(index)} className="input-group-text">Delete ToDo</button>
+        </ul>
+    );
 }
 
 
